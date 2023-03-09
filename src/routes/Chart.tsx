f84259d33134d404cf723090d81f5cea722dc8ed
phjjj <aaagowns@gmail.com>
2023-03-10 01:11:25 +0900

    1
    1

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,11 @@
+
+interface ChartProps {
+  coinId: string;
+}
+function Chart({coinId}: ChartProps) {
+ 
+
+  return <h1>Chart</h1>;
+}
+
+export default Chart;
---
+++ src/routes/Price.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router";
+
+function Price() {
+ 
+  return <h1>Price</h1>;
+}
+
+export default Price;
